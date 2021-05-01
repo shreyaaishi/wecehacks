249839b3ad8f3f4afdbf4c7bf5c274d431a86343
Shreya Basu <shreyab4@illinois.edu>
2021-05-01 10:23:30 -0500

    Created basic React app

---
+++ wecehacks-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          //onChange={handleChange}
+        />
+{/*         <button onClick={getMealData}>Get Daily Meal Plan</button>
+      {mealData && <MealList mealData={mealData} />} */}
+    </section>
+    </div>
+  );
+}
+
+export default App;
